b38d834c32ac07f9b2c9642bf6bde3493ed944ed
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-01 09:20:47 -0700

    add get token script

---
+++ spotipyxx.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import json
+import spotipy
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+
+def get_token():
+    #Get username from terminal
+    username = sys.argv[1]
+
+
+    #try:
+    token = util.prompt_for_user_token(username, 
+                                        scope='user-top-read user-library-read', 
+                                        client_id='87e89d824a3046ad9d2c6fcf05931cab', 
+                                        client_secret='e9001d177fcb4071bad33987520f5f16', 
+                                        redirect_uri='https://www.google.com/')
+    return token
+    #except:
+    #    os.remove(f".cache-{username}")
+
+
+    #spotifyObject = spotipy.Spotify(auth=token)
