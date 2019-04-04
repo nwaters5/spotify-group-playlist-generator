3a016181e61ea069fd7f2af2d4c4764e6acee622
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-04 14:44:47 -0700

    now adds playlist to user account

--- create_two_user_playlist.py
+++ create_two_user_playlist.py
@@ -1,8 +1,10 @@
 import two_user_recommender
-import random
+from random import shuffle
+import spotipyxx
+import spotipy
 rec = two_user_recommender.TwoUserRecommender()
 
-def create(user1, user2):
+def create(user1, user2, playlist_name):
     user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile = rec.get_data(user1, user2)
     rec.initialize(user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile)
     rec.fit()
@@ -10,10 +12,13 @@ def create(user1, user2):
     rec.initialize(user2_playlist, user2_library, user2_profile, user1_playlist, user1_library, user1_profile)
     rec.fit()
     d2 = rec.get_recommended_playlist()
-    res = {**d1, **d2}
-    keys = res.keys()
-    random.shuffle(keys)
-    new_res = {}
-    for key in keys:
-        new_res.update({key, res[key]})
-    return new_res
+    res = d1 + d2
+    shuffle(res)
+    token = spotipyxx.get_token(user1)
+    sp = spotipy.Spotify(auth=token)
+    sp.user_playlist_create(user1, playlist_name)
+    final_playlist = sp.user_playlist_create(user=user1, name=playlist_name)
+    sp.user_playlist_add_tracks(user=user1, 
+                             playlist_id=final_playlist['id'], 
+                             tracks=res)
+
--- spotipyxx.py
+++ spotipyxx.py
@@ -13,7 +13,7 @@ def get_token(username):
 
     #try:
     token = util.prompt_for_user_token(username, 
-                                        scope='user-top-read user-library-read', 
+                                        scope='user-top-read user-library-read playlist-modify-public', 
                                         client_id='87e89d824a3046ad9d2c6fcf05931cab', 
                                         client_secret='e9001d177fcb4071bad33987520f5f16', 
                                         redirect_uri='https://www.google.com/')
