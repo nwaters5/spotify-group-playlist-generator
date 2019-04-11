5c4fbd77b8cb8b535a6a1d397ea7a29776760ae9
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-11 13:59:23 -0700

    changes

--- two_user_recommender.py
+++ two_user_recommender.py
@@ -5,6 +5,7 @@ import operator
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import preprocessing
 
+
 class TwoUserRecommender(object):
     
     def __init__(self, user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile):
@@ -15,6 +16,9 @@ class TwoUserRecommender(object):
         self.user2_library = user2_library
         self.user2_profile = user2_profile
         self.all_users_songs = pd.concat([user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile])
+        self.new_songs = pd.concat([pd.load_pickle('final_df.pkl'), self.user2_profile]).fillna(0)
+
+
     def fit(self):
         #for i in self.user1_playlist.columns:
         #    if i not in self.user2_profile.columns:
@@ -32,7 +36,7 @@ class TwoUserRecommender(object):
         x = matrix2.values 
         min_max_scaler = preprocessing.MinMaxScaler()
         x_scaled = min_max_scaler.fit_transform(x)
-        self.matrix3 = pd.DataFrame(x_scaled)
+        self.matrix3 = pd.DataFrame(x_scaled, columns=matrix2.columns, index=matrix2.index)
         self.cosine_sim = cosine_similarity(self.matrix3, self.matrix3)
         return self
     
@@ -75,37 +79,23 @@ class TwoUserRecommender(object):
         #    new_res.append(key)
         return res #, new_res
 
-'''       
+'''   
     def recommend_new_songs(self, title):
-        new_songs = pd.read_csv()
-        new_songs = pd.concat([new_songs, self.user2_profile]).fillna(0)
-        new_songs.set_index('track_uri', drop=True, inplace=True)
-        new_songs2 = new_songs.drop(columns=['artist_uri', 'track', 'artist']).fillna(0)
-        cos_sim_new = cosine_similarity(new_songs2, new_songs2)
-        indices = pd.Series(new_songs2.index)
+        self.new_songs.set_index('track_uri', drop=True, inplace=True)
+        cos_sim_new = cosine_similarity(self.new_songs.drop(columns=['artist_uri', 'track', 'artist']).fillna(0), 
+                                        self.new_songs.drop(columns=['artist_uri', 'track', 'artist']).fillna(0))
+        indices = pd.Series(self.new_songs.index)
         recommended_songs = {}
         idx = indices[indices == title].index[0]
         score_series = pd.Series(cos_sim_new[idx]).sort_values(ascending = False)
         for i in range(1, 100):
             j = list(score_series.iloc[i:i+1].index)[0]
             score = list(score_series.iloc[i:i+1])[0]
-            if list(new_songs2.index)[j] not in (list(self.all_users_songs['track_uri'])):
-                if str(new_songs.at[list(new_songs.index)[j], 'artist_uri']) not in list(self.user2_profile[self.user2_profile['track_uri'] == title]['artist_uri']):
-                    recommended_songs[list(new_songs2.index)[j]] =  score
+            if list(self.new_songs.index)[j] not in (list(self.all_users_songs['track_uri'])):
+                if str(self.new_songs.at[list(self.new_songs.index)[j], 'artist_uri']) not in list(self.new_songs[self.new_songs['track_uri'] == title]['artist_uri']):
+                    recommended_songs[list(self.new_songs.index)[j]] =  score
             if len(recommended_songs) > 5:
                 break
         return recommended_songs
 
-'''
-        
-
-
-
-
-
-
-
-
-
-
-
+'''  
