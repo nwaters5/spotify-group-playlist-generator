2dd67ece09edc06aa119b54f67cdf626a781e0f0
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-11 20:33:48 -0700

    so no same songs - may have to address

--- create_two_user_playlist.py
+++ create_two_user_playlist.py
@@ -1,12 +1,19 @@
 import two_user_recommender
-import get_user_data
 from random import shuffle
 import spotipyxx
 import spotipy
+import scrape_user_library as scrape
+
+def get_user_data(username):
+    s = scrape.ScrapeUserLibrary(username)
+    library = s.get_library_df()
+    playlists = s.get_playlist_df()
+    top_tracks = s.get_top_tracks()
+    return playlists.fillna(0), library.fillna(0), top_tracks.fillna(0)
 
 def get_data(user1, user2):
-    user1_playlist, user1_library, user1_profile = get_user_data.get_user_data(user1)
-    user2_playlist, user2_library, user2_profile = get_user_data.get_user_data(user2)
+    user1_playlist, user1_library, user1_profile = get_user_data(user1)
+    user2_playlist, user2_library, user2_profile = get_user_data(user2)
     return user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile
 
 def create(user1, user2, playlist_name):
@@ -17,7 +24,7 @@ def create(user1, user2, playlist_name):
     rec = two_user_recommender.TwoUserRecommender(user2_playlist, user2_library, user2_profile, user1_playlist, user1_library, user1_profile)
     rec.fit()
     d2 = rec.get_recommended_playlist()
-    res = d1 + d2
+    res = list(set(d1 + d2))
     shuffle(res)
     new = rec.recommend_new_songs(res)
     shuffle(new)
