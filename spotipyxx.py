241233ba2c28588aebcc2d8cc7fe63f55b00b8b6
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-15 11:00:10 -0700

    security

--- spotipyxx.py
+++ spotipyxx.py
@@ -4,7 +4,7 @@ import json
 import spotipy
 import spotipy.util as util
 from json.decoder import JSONDecodeError
-
+import config
 
 def get_token(username):
     #Get username from terminal
@@ -14,8 +14,8 @@ def get_token(username):
     #try:
     token = util.prompt_for_user_token(username, 
                                         scope='user-top-read user-library-read playlist-modify-public', 
-                                        client_id='87e89d824a3046ad9d2c6fcf05931cab', 
-                                        client_secret='e9001d177fcb4071bad33987520f5f16', 
+                                        client_id=config.client_id, 
+                                        client_secret=config.client_secret, 
                                         redirect_uri='http://127.0.0.1:5000/')
     return token
     #except:
