3ce647179d9957049276956d37366550d16b6ca9
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-17 12:30:14 -0700

    clean up, comment

--- create_two_user_playlist.py
+++ create_two_user_playlist.py
@@ -1,9 +1,9 @@
 import two_user_recommender
 from random import shuffle
-#import spotipyxx
 import spotipy
 import scrape_user_library as scrape
 
+# get user's library, playlists, top tracks
 def get_user_data(token):
     s = scrape.ScrapeUserLibrary(token)
     library = s.get_library_df()
@@ -11,30 +11,58 @@ def get_user_data(token):
     top_tracks = s.get_top_tracks()
     return playlists.fillna(0), library.fillna(0), top_tracks.fillna(0)
 
+
+# get both users' library, playlists, top tracks
 def get_data(token1, token2):
     user1_playlist, user1_library, user1_profile = get_user_data(token1)
     user2_playlist, user2_library, user2_profile = get_user_data(token2)
-    return user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile
+    return (
+        user1_playlist,
+        user1_library,
+        user1_profile,
+        user2_playlist,
+        user2_library,
+        user2_profile,
+    )
 
+
+# get recommendations from each users' playlists
+# use those recommendations to get 10 new song recommendations
+# create playlist
 def create(token1, token2, playlist_name):
-    user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile = get_data(token1, token2)
-    rec = two_user_recommender.TwoUserRecommender(user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile)
+    user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile = get_data(
+        token1, token2
+    )
+    rec = two_user_recommender.TwoUserRecommender(
+        user1_playlist,
+        user1_library,
+        user1_profile,
+        user2_playlist,
+        user2_library,
+        user2_profile,
+    )
     rec.fit()
     d1 = rec.get_recommended_playlist()
-    rec = two_user_recommender.TwoUserRecommender(user2_playlist, user2_library, user2_profile, user1_playlist, user1_library, user1_profile)
+    rec = two_user_recommender.TwoUserRecommender(
+        user2_playlist,
+        user2_library,
+        user2_profile,
+        user1_playlist,
+        user1_library,
+        user1_profile,
+    )
     rec.fit()
     d2 = rec.get_recommended_playlist()
     res = d1 + d2
-    #shuffle(res)
+    # shuffle(res)
     new = rec.recommend_new_songs(res)
     shuffle(new)
-    final_playlist = res + new[:15]
-    #token = spotipyxx.get_token(user1)
+    final_playlist = res + new[:10]
     sp = spotipy.Spotify(auth=token1)
-    user1 = sp.current_user()['id']
-    #sp.user_playlist_create(user1, playlist_name)
-    #playlist_id = str(sp.user_playlist_create(user=user1, name=playlist_name)['id'])
-    sp.user_playlist_add_tracks(user=user1, 
-                                playlist_id=sp.user_playlist_create(user=user1, name=playlist_name)['id'], 
-                                tracks=final_playlist)
+    user1 = sp.current_user()["id"]
+    sp.user_playlist_add_tracks(
+        user=user1,
+        playlist_id=sp.user_playlist_create(user=user1, name=playlist_name)["id"],
+        tracks=final_playlist,
+    )
 
--- two_user_recommender.py
+++ two_user_recommender.py
@@ -1,50 +1,77 @@
-import get_user_data
 import scrape_user_library as scrape
 import pandas as pd
 import operator
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import preprocessing
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.neighbors import NearestNeighbors
 import pickle
 
+
 class TwoUserRecommender(object):
-    
-    def __init__(self, user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile):
+    # initialize both users' tracks
+    def __init__(
+        self,
+        user1_playlist,
+        user1_library,
+        user1_profile,
+        user2_playlist,
+        user2_library,
+        user2_profile,
+    ):
         self.user1_playlist = user1_playlist
         self.user1_library = user1_library
         self.user1_profile = user1_profile
         self.user2_playlist = user2_playlist
         self.user2_library = user2_library
         self.user2_profile = user2_profile
-        self.all_users_songs = pd.concat([user1_playlist, user1_library, user1_profile, user2_playlist, user2_library, user2_profile], sort=True)
-        self.play_prof = pd.concat([user1_playlist, user1_profile, user2_playlist, user2_profile], sort=True)
+        self.all_users_songs = pd.concat(
+            [
+                user1_playlist,
+                user1_library,
+                user1_profile,
+                user2_playlist,
+                user2_library,
+                user2_profile,
+            ],
+            sort=True,
+        )
+        self.play_prof = pd.concat(
+            [user1_playlist, user1_profile, user2_playlist, user2_profile], sort=True
+        )
 
+    # build cosine similarity matrix of user's songs
     def fit(self):
-        self.matrix = pd.concat([self.user1_playlist, self.user2_profile, self.user1_profile], sort=True)
+        self.matrix = pd.concat(
+            [self.user1_playlist, self.user2_profile, self.user1_profile], sort=True
+        )
         self.matrix.drop_duplicates(inplace=True)
-        self.matrix.set_index('track_uri', drop=True, inplace=True)
-        matrix2 = self.matrix.drop(columns=['artist_uri', 'track', 'artist']).fillna(0)
-        x = matrix2.values 
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x_scaled = min_max_scaler.fit_transform(x)
-        self.matrix3 = pd.DataFrame(x_scaled, columns=matrix2.columns, index=matrix2.index)
-        self.cosine_sim = cosine_similarity(self.matrix3, self.matrix3)
+        self.matrix.set_index("track_uri", drop=True, inplace=True)
+        matrix2 = self.matrix.drop(columns=["artist_uri", "track", "artist"]).fillna(0)
+        x = matrix2.values
+        x_scaled = preprocessing.MinMaxScaler().fit_transform(x)
+        self.std_matrix = pd.DataFrame(
+            x_scaled, columns=matrix2.columns, index=matrix2.index
+        )
+        self.cosine_sim = cosine_similarity(self.std_matrix, self.std_matrix)
         return self
-    
+
+    # recommend user2 songs from user1's playlists/profile
     def recommend(self, title):
-        indices = pd.Series(self.matrix3.index)
+        indices = pd.Series(self.std_matrix.index)
         recommended_songs = {}
         idx = indices[indices == title].index[0]
-        score_series = pd.Series(self.cosine_sim[idx]).sort_values(ascending = False)
-        #top_40_indexes = list(score_series.iloc[1:41].index)
-        #top_40_scores = list(score_series.iloc[1:41])
+        score_series = pd.Series(self.cosine_sim[idx]).sort_values(ascending=False)
         artists = []
-        title_artist = list(self.user2_profile[self.user2_profile['track_uri'] == title]['artist_uri'])
+        title_artist = list(
+            self.user2_profile[self.user2_profile["track_uri"] == title]["artist_uri"]
+        )
         for i in range(1, 100):
-            j = list(score_series.iloc[i:i+1].index)[0]
-            score = list(score_series.iloc[i:i+1])[0]
-            rec_artist = str(self.matrix.at[list(self.matrix.index)[j], 'artist_uri'])
-            if list(self.matrix3.index)[j] not in (list(self.user2_profile['track_uri'])):# + list(self.user2_library['track_uri']) + list(self.user2_playlist['track_uri'])):
+            j = list(score_series.iloc[i : i + 1].index)[0]
+            score = list(score_series.iloc[i : i + 1])[0]
+            rec_artist = str(self.matrix.at[list(self.matrix.index)[j], "artist_uri"])
+            if list(self.std_matrix.index)[j] not in (
+                list(self.user2_profile["track_uri"])
+            ):  # + list(self.user2_library['track_uri']) + list(self.user2_playlist['track_uri'])):
                 if rec_artist not in (title_artist + artists):
                     recommended_songs[list(self.matrix.index)[j]] = score
                     artists.append(rec_artist)
@@ -52,47 +79,46 @@ class TwoUserRecommender(object):
                 break
         return recommended_songs
 
-
+    # take top 10 recommendations and put them in a list
     def get_recommended_playlist(self):
         final_recs = {}
-        for title in self.user2_profile['track_uri']:
+        for title in self.user2_profile["track_uri"]:
             d = self.recommend(title)
             final_recs = {**final_recs, **d}
-        newA = sorted(final_recs, key=final_recs.get, reverse=True)[:15]
+        newA = sorted(final_recs, key=final_recs.get, reverse=True)[:10]
         res = list()
         for key in newA:
             res.append(key)
         return res
 
+    # using the song dataset, recommend songs that are in neither user's library
+    # based on the 10 previous recommendations
+    # NearestNeighbors model
     def recommend_new_songs(self, songs):
-        #model = pickle.load(open("model_30n.pkl", 'rb'))
-        df1 = pd.read_pickle('unstandardized_with_artists.pkl')
-        self.play_prof.set_index('track_uri', drop=True, inplace=True)
-        entire = self.play_prof.drop(columns=['track', 'artist']).fillna(0)
-        df = pd.concat([df1, entire]).fillna(0)
-        #df.to_pickle('unstandardized_with_artists_updated.pkl')
-        artist_checker = df[['artist_uri']]
-        df = df.drop(columns='artist_uri')
-        df = df[~df.index.duplicated(keep='first')]
+        df1 = pd.read_pickle("unstandardized_with_artists.pkl")
+        self.play_prof.set_index("track_uri", drop=True, inplace=True)
+        entire = self.play_prof.drop(columns=["track", "artist"]).fillna(0)
+        df = pd.concat([df1, entire], sort=True).fillna(0)
+        # df.to_pickle('unstandardized_with_artists_updated.pkl')
+        artist_checker = df[["artist_uri"]]
+        df = df[~df.index.duplicated(keep="first")].drop(columns="artist_uri")
         x = df.values
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x_scaled = min_max_scaler.fit_transform(x)
+        x_scaled = preprocessing.MinMaxScaler().fit_transform(x)
         std_df = pd.DataFrame(x_scaled, columns=df.columns, index=df.index)
         X = std_df.values
         model = NearestNeighbors(n_neighbors=30).fit(X)
-        #artist_checker = pd.read_pickle('artist_checker.pkl')
         final_recommendations = []
         for song in songs:
             t = std_df[std_df.index == song].values[0]
             l = list(df.iloc[model.kneighbors([t])[1][0]].index)
             recommendations = []
             artists = []
-            title_artist = str(artist_checker.at[song, 'artist_uri'])
+            title_artist = str(artist_checker.at[song, "artist_uri"])
             artists.append(title_artist)
             for rec in l:
-                if rec not in list(self.all_users_songs['track_uri']):
-                    rec_artist = str(artist_checker.at[rec, 'artist_uri'])
-                    if (rec_artist not in artists and rec not in songs):
+                if rec not in list(self.all_users_songs["track_uri"]):
+                    rec_artist = str(artist_checker.at[rec, "artist_uri"])
+                    if rec_artist not in artists and rec not in songs:
                         recommendations.append(rec)
                         artists.append(rec_artist)
                 if len(recommendations) > 2:
