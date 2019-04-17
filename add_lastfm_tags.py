55900d93ce70110980087d94355c8838aee44022
Nick Waters <nick@Nicks-MacBook-Pro.local>
2019-04-17 15:00:18 -0700

    each track tag as column

--- add_lastfm_tags.py
+++ add_lastfm_tags.py
@@ -3,30 +3,40 @@ import pylast
 
 username = "dummy_account_5"
 password_hash = pylast.md5("dummypassword1!")
-network = pylast.LastFMNetwork(api_key='ba7e56c3cc988219ffb50a6dede0093a', api_secret='4fcfea7e8068651b411f5e343ce718f5',
-                               username=username, password_hash=password_hash)
+network = pylast.LastFMNetwork(
+    api_key="52557fe79157ddf53597b835faa25f92",
+    api_secret="3a018cc75468cadeaa9fbfa4604dba5e",
+    username=username,
+    password_hash=password_hash,
+)
 
-#returns dictionary of top 10 tags of artist and their weight
-def get_tags(artist):
-    band = network.get_artist(artist)
-    topItems = band.get_top_tags(limit=10)
-    d = {}
+# returns dictionary of top 10 tags of artist and their weight
+def get_tags(artist, song):
+    track = network.get_track(artist, song)
+    topItems = track.get_top_tags(limit=8)
+    d = []
     for topItem in topItems:
-        if "".join((str(topItem.item.get_name()).lower()).split()) != "".join((str(artist).lower()).split()):
-            item_clean = "".join(topItem.item.get_name().lower().replace("-", "").split())
-            d.update({item_clean: topItem.weight})    
+        if "".join((str(topItem.item.get_name()).lower()).split()) != "".join(
+            (str(artist).lower()).split()
+        ):
+            item_clean = "".join(
+                topItem.item.get_name().lower().replace("-", "").split()
+            )
+            d.append(topItem.item.get_name().lower())
     return d
 
-#adds the top tags to dataframe
+
+# adds the top tags to dataframe
 def add_features_to_df(df):
+    df["lastfm"] = 0
     for i, row in df.iterrows():
         try:
-            d = get_tags(row['artist'])
+            d = get_tags(row["artist"], row["track"])
         except:
-            continue    
+            continue
         for key, value in d.items():
             df.at[i, key] = value
-    #drops uncommon columns
+    # drops uncommon columns
     for column in df:
         if df[column].count() <= 1:
             df.drop(columns=column, inplace=True)
